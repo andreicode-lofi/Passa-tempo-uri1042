4030732171e1b926b5d4c0a522c90ddde5814b57
Andrey Souza <andreycode@hotmail.com>
2021-06-24 18:13:46 -0300

    resolvendo problemas da plataforma uri online

---
+++ Uri1042/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Uri1042
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int menor  =0, meio=0 , maior=0 ;
+
+            string[] vet = Console.ReadLine().Split(' ');
+
+
+            int a = int.Parse(vet[0]);
+            int b = int.Parse(vet[1]);
+            int c = int.Parse(vet[2]);
+
+            if(a < b && a < c)
+            {
+                menor = a;
+                if (b < c )
+                {
+                    meio = b;
+                    menor = c;
+                }
+                else
+                {
+                    meio = c;
+                    menor = b;
+                }
+            }
+
+            if(b>a && b > c)
+            {
+                maior = b;
+                if (c > a)
+                {
+                    meio = c;
+                    menor = a;
+                }
+                else
+                {
+                    meio = a;
+                    menor = c;
+                }
+            }
+
+            if(c>a && c > b)
+            {
+                maior = c;
+                if (a < b)
+                {
+                    meio = b;
+                    menor = a;
+                }
+                else
+                {
+                    meio = a;
+                    menor = b;
+                }
+            }
+
+            Console.WriteLine(menor);
+            Console.WriteLine(meio);
+            Console.WriteLine(maior);
+
+            Console.WriteLine();
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+
+
+
+
+
+        }
+    }
+}
